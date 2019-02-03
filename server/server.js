4fec9d4730cf574060c83ffa5d2d7f51e9805305
darrenrector91 <darrenrector91@gmail.com>
2019-02-02 22:52:45 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("server/public"));
+
+// Start listening for requests on a specific port
+app.listen(process.env.PORT || port, function () {
+    console.log("listening on port", process.env.PORT || 5000);
+});
